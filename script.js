eef05e90153c292def2843305be2166b1ec44ff8
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-03-27 16:08:00 +0200

    Ajout fonctionnalité, case écrit intouchable

--- script.js
+++ script.js
@@ -11,20 +11,27 @@ for(i = 0; i < 9; i++){
     contenu.appendChild(nouvCase);
     nouvCase.addEventListener("click", function(){
         if(joueurActuel === "✗"){
-            let croix = document.createElement("span");
-            nouvCase.appendChild(croix);
-            croix.classList.add("croix");
-            croix.innerHTML = "✗";
-            joueurActuel = "◯";
-            texteTour.innerHTML = "◯";
+            if(!nouvCase.hasChildNodes()){
+                let croix = document.createElement("span");
+                nouvCase.appendChild(croix);
+                croix.classList.add("croix");
+                croix.innerHTML = "✗";
+                nouvCase.style.cursor = "not-allowed";
+                joueurActuel = "◯";
+                texteTour.innerHTML = "◯";
+            }
+            
         }
         else{
-            let rond = document.createElement("span");
-            nouvCase.appendChild(rond);
-            rond.classList.add("rond");
-            rond.innerHTML = "◯";
-            joueurActuel = "✗";
-            texteTour.innerHTML = "✗";
+            if(!nouvCase.hasChildNodes()){
+                let rond = document.createElement("span");
+                nouvCase.appendChild(rond);
+                rond.classList.add("rond");
+                rond.innerHTML = "◯";
+                nouvCase.style.cursor = "not-allowed";
+                joueurActuel = "✗";
+                texteTour.innerHTML = "✗";
+            }
         }
     })
 }
