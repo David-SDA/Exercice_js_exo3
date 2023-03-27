5c862b438314be572becbab87e229fd8fe754dd7
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-03-27 15:36:57 +0200

    Ajout alternance entre joueurs

--- script.js
+++ script.js
@@ -8,4 +8,20 @@ caseDeJeu.classList.add("case");
 for(i = 0; i < 9; i++){
     let nouvCase = caseDeJeu.cloneNode();
     contenu.appendChild(nouvCase);
+    nouvCase.addEventListener("click", function(){
+        if(joueurActuel === "✗"){
+            let croix = document.createElement("span");
+            nouvCase.appendChild(croix);
+            croix.classList.add("croix");
+            croix.innerHTML = "✗";
+            joueurActuel = "◯";
+        }
+        else{
+            let rond = document.createElement("span");
+            nouvCase.appendChild(rond);
+            rond.classList.add("rond");
+            rond.innerHTML = "◯";
+            joueurActuel = "✗";
+        }
+    })
 }
