1a88afc26f5808183e8003e40a9f254892ab86c2
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-03-28 09:46:11 +0200

    Ajout de commentaires

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-const contenu = document.querySelector(".contenu");
-const texteTour = document.querySelector(".tourJoueur");
+/* Les constantes */
+const contenu = document.querySelector(".contenu"); // le plan de jeu
+const texteTour = document.querySelector(".tourJoueur"); // le caractère qui détermine le tour du joueur
 
-let joueurActuel = "✗"; // ou ◯
-let nombreTotalTour = 0;
-let etatJeu = ["", "", "", "", "", "", "", "", ""];
+/* Les variables */
+let joueurActuel = "✗" /* ou ◯ */; // le joueur qui doit jouer
+let nombreTotalTour = 0; // le nombre de tour écoulés
+let etatJeu = ["", "", "", "", "", "", "", "", ""]; // tableau pour suivre l'évolution du jeu
 
+/* Création d'une case de base */
 const caseDeJeu = document.createElement("div");
 caseDeJeu.classList.add("case");
 
+/* Fonction qui vérifie tout les cas possible de victoire */
 function verifierGagnant(){
     if((etatJeu[0] == etatJeu[1] && etatJeu[1] == etatJeu[2] && (etatJeu[0] !== "") && (etatJeu[1] !== "") && (etatJeu[2] !== "")) ||
     (etatJeu[3] == etatJeu[4] && etatJeu[4] == etatJeu[5] && (etatJeu[3] !== "") && (etatJeu[4] !== "") && (etatJeu[5] !== "")) ||
@@ -24,43 +28,44 @@ function verifierGagnant(){
     }
 }
 
+/* Création des cases avec leurs fonctionnalités */
 for(i = 0; i < 9; i++){
-    let nouvCase = caseDeJeu.cloneNode();
-    nouvCase.id = i;
-    contenu.appendChild(nouvCase);
-    nouvCase.addEventListener("click", function(){
-        if(joueurActuel === "✗"){
-            if(!nouvCase.hasChildNodes()){
-                let croix = document.createElement("span");
+    let nouvCase = caseDeJeu.cloneNode(); // clone d'une case
+    nouvCase.id = i; // ajout d'un id, utile pour la corrélation avec le tableau
+    contenu.appendChild(nouvCase); // création de la case
+    nouvCase.addEventListener("click", function(){ //ajout de fonctionnalité lors du click d'un case
+        if(joueurActuel === "✗"){ // le joueur est ✗
+            if(!nouvCase.hasChildNodes()){ // si dans la case, il n'y a rien d'écrit
+                let croix = document.createElement("span"); // on crée un span
                 nouvCase.appendChild(croix);
-                croix.classList.add("croix");
-                croix.innerHTML = "✗";
-                nouvCase.style.cursor = "not-allowed";
-                joueurActuel = "◯";
-                texteTour.innerHTML = "◯";
-                nombreTotalTour++;
-                var id = nouvCase.getAttribute("id");
-                etatJeu[id] = "✗";
-                if(verifierGagnant()){
+                croix.classList.add("croix"); // on ajoute son style
+                croix.innerHTML = "✗"; // ainsi que son texte
+                nouvCase.style.cursor = "not-allowed"; // on indique au joueur que la case est injouable
+                joueurActuel = "◯"; // changement de joueur
+                texteTour.innerHTML = "◯"; // qu'on affiche
+                nombreTotalTour++; // on increment le nombre de tours
+                var id = nouvCase.getAttribute("id"); // on recupère l'id (qui est un chiffre)
+                etatJeu[id] = "✗"; // corrélation entre l'état du plan de jeu et son tableau 
+                if(verifierGagnant()){ // on vérifie si il y a un gagnant
                     setTimeout(function(){
-                        alert("Le joueur ✗ a gagné !");
+                        alert("Le joueur ✗ a gagné !"); // on dit qu'il a gagné
                     }, 100);
                     setTimeout(function(){
-                        window.location.reload();
+                        window.location.reload(); // puis on recharge la page pour recommencer un partie
                     }, 1000);
                 }
-                if(nombreTotalTour == 9){
+                if(nombreTotalTour == 9){ // pas de gagnant -> on a complété toute les cases
                     setTimeout(function(){
-                        alert("Partie terminée !");
+                        alert("Partie terminée !"); // donc la partie est terminée
                     }, 100);
                     setTimeout(function(){
-                        window.location.reload();
+                        window.location.reload(); // on rechage la page pour recommencer une partie
                     }, 1000);
                 }
             }
             
         }
-        else{
+        else{ // on fait la même chose que plus haut, sauf que le joueur est ◯
             if(!nouvCase.hasChildNodes()){
                 let rond = document.createElement("span");
                 nouvCase.appendChild(rond);
