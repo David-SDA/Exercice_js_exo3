87341a81468eace23bc2342e6c523c0b825c3c96
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-03-28 09:24:52 +0200

    Ajout de fonctionnalité, vérification d'un gagnant

--- script.js
+++ script.js
@@ -8,9 +8,25 @@ let etatJeu = ["", "", "", "", "", "", "", "", ""];
 const caseDeJeu = document.createElement("div");
 caseDeJeu.classList.add("case");
 
+function verifierGagnant(){
+    if((etatJeu[0] == etatJeu[1] && etatJeu[1] == etatJeu[2] && (etatJeu[0] !== "") && (etatJeu[1] !== "") && (etatJeu[2] !== "")) ||
+    (etatJeu[3] == etatJeu[4] && etatJeu[4] == etatJeu[5] && (etatJeu[3] !== "") && (etatJeu[4] !== "") && (etatJeu[5] !== "")) ||
+    (etatJeu[6] == etatJeu[7] && etatJeu[7] == etatJeu[8] && (etatJeu[6] !== "") && (etatJeu[7] !== "") && (etatJeu[8] !== "")) ||
+    (etatJeu[0] == etatJeu[3] && etatJeu[3] == etatJeu[6] && (etatJeu[0] !== "") && (etatJeu[3] !== "") && (etatJeu[6] !== "")) ||
+    (etatJeu[1] == etatJeu[4] && etatJeu[4] == etatJeu[7] && (etatJeu[1] !== "") && (etatJeu[4] !== "") && (etatJeu[7] !== "")) ||
+    (etatJeu[2] == etatJeu[5] && etatJeu[5] == etatJeu[8] && (etatJeu[2] !== "") && (etatJeu[5] !== "") && (etatJeu[8] !== "")) ||
+    (etatJeu[0] == etatJeu[4] && etatJeu[4] == etatJeu[8] && (etatJeu[0] !== "") && (etatJeu[4] !== "") && (etatJeu[8] !== "")) ||
+    (etatJeu[2] == etatJeu[4] && etatJeu[4] == etatJeu[6] && (etatJeu[2] !== "") && (etatJeu[4] !== "") && (etatJeu[6] !== ""))){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
 for(i = 0; i < 9; i++){
     let nouvCase = caseDeJeu.cloneNode();
-    nouvCase.id = i+1;
+    nouvCase.id = i;
     contenu.appendChild(nouvCase);
     nouvCase.addEventListener("click", function(){
         if(joueurActuel === "✗"){
@@ -23,7 +39,14 @@ for(i = 0; i < 9; i++){
                 joueurActuel = "◯";
                 texteTour.innerHTML = "◯";
                 nombreTotalTour++;
-                console.log(etatJeu);
+                var id = nouvCase.getAttribute("id");
+                etatJeu[id] = "✗";
+                if(verifierGagnant()){
+                    setTimeout(function(){
+                        alert("Le joueur ✗ a gagné !");
+                    }, 100);
+                    
+                }
                 if(nombreTotalTour == 9){
                     setTimeout(function(){
                         alert("Partie terminée !");
@@ -42,6 +65,13 @@ for(i = 0; i < 9; i++){
                 joueurActuel = "✗";
                 texteTour.innerHTML = "✗";
                 nombreTotalTour++;
+                var id = nouvCase.getAttribute("id");
+                etatJeu[id] = "◯";
+                if(verifierGagnant()){
+                    setTimeout(function(){
+                        alert("Le joueur ◯ a gagné !");
+                    }, 100);
+                }
                 if(nombreTotalTour == 9){
                     setTimeout(function(){
                         alert("Partie Terminée !");
